d9342d4d26c62932fa67695035b214d138be018e
Ayothegod <heyayomideadebisi@gmail.com>
2024-12-11 14:23:31 +0100

    config mishka buttons

--- lib/trivia_web/live/default_live/index.ex
+++ lib/trivia_web/live/default_live/index.ex
@@ -1,8 +1,11 @@
 defmodule TriviaWeb.DefaultLive.Index do
   use TriviaWeb, :live_view
 
+  # import TriviaWeb.Components.Button
+
   # alias Trivia.Arenas
   # alias Trivia.Arenas.Arena
+  alias TriviaWeb.Components.Button, as: Button
 
   @impl true
   def mount(_params, _session, socket) do
