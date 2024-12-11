1851c6e12d33afffc317a7261c3d7887932486e5
Ayothegod <heyayomideadebisi@gmail.com>
2024-12-11 19:19:18 +0100

    remove all migrations files

--- priv/repo/migrations/20241211181342_remove_past_achievements_column.exs
+++
@@ -1,9 +0,0 @@
-defmodule Trivia.Repo.Migrations.RemovePastAchievementsColumn do
-  use Ecto.Migration
-
-  def change do
-    alter table(:user_profile) do
-      remove :past_achievements
-    end
-  end
-end
